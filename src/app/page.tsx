17528ebe5dd7de9a439c74bc38e7ccf5bbc9ea13
joaogamin <joaumxp@hotmail.com>
2024-10-10 17:06:41 -0300

    jgamin : feat initial user interface visual structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,106 @@
-export default function HomePage() {
+import {
+  DotsThreeVertical,
+  PlusCircle,
+  Spinner,
+  Video,
+} from '@phosphor-icons/react/dist/ssr'
+
+export default async function Home() {
   return (
-    <h1>
-      Hello
-      World!
-    </h1>
+    <div className="h-screen bg-zinc-950 text-zinc-50 flex flex-row-reverse justify-center items-center">
+      <div className="flex max-w-[1100px] w-full mx-auto px-5 2xl:px-0 flex-col gap-6">
+        <div className="flex items-center justify-between ">
+          <div className="flex flex-col gap-1">
+            <h1 className="text-2xl font-bold">
+              Título
+              do
+              vídeo
+            </h1>
+            <span className="text-sm text-zinc-400">
+              Módulo
+              do
+              vídeo
+            </span>
+          </div>
+
+          <button className="flex items-center gap-2 rounded bg-blue-800 px-3 py-2 text-sm font-medium text-white hover:bg-blue-900">
+            <PlusCircle className="w-4 h-4" />
+            Adicionar
+            vídeo
+          </button>
+        </div>
+
+        <main className="relative flex flex-col gap-1 md:flex-row overflow-hidden rounded-lg border border-zinc-800 bg-zinc-900 shadow md:pr-80">
+          <div className="md:flex-1">
+            <div className="w-full bg-zinc-950 aspect-video">
+              <div className="flex h-full items-center justify-center">
+                <Spinner className="w-6 h-6 text-zinc-400 animate-spin" />
+              </div>
+            </div>
+          </div>
+          <div className="md:w-80 w-full divide-y-2 divide-zinc-900 border-l border-zinc-800 bg-zinc-900 md:absolute md:top-2 md:bottom-0 md:right-0 md:overflow-y-scroll scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-900">
+            <h2 className="text-zinc-50 text-lg border-b border-zinc-800 px-4 py-2">
+              Listagem
+              de
+              vídeos
+            </h2>
+            <div className="flex items-center flex-row">
+              <button className="flex items-center justify-between w-full pl-4 pr-2 py-2 gap-3 text-sm text-zinc-400 data-[active=true]:text-emerald-400 enabled:hover:text-zinc-100">
+                <div className="flex items-center gap-4">
+                  <Video className="w-4 h-4 text-zinc-500" />
+
+                  <span>
+                    Vídeo
+                    1
+                  </span>
+                </div>
+                <span className="ml-auto font-mono text-xs text-zinc-500">
+                  03:54
+                </span>
+              </button>
+              <button className="pr-2">
+                <DotsThreeVertical className="w-4 h-4 text-zinc-500" />
+              </button>
+            </div>
+            <div className="flex items-center flex-row">
+              <button className="flex items-center justify-between w-full pl-4 pr-2 py-2 gap-3 text-sm text-zinc-400 data-[active=true]:text-emerald-400 enabled:hover:text-zinc-100">
+                <div className="flex items-center gap-4">
+                  <Video className="w-4 h-4 text-zinc-500" />
+
+                  <span>
+                    Vídeo
+                    1
+                  </span>
+                </div>
+                <span className="ml-auto font-mono text-xs text-zinc-500">
+                  03:54
+                </span>
+              </button>
+              <button className="pr-2">
+                <DotsThreeVertical className="w-4 h-4 text-zinc-500" />
+              </button>
+            </div>
+            <div className="flex items-center flex-row">
+              <button className="flex items-center justify-between w-full pl-4 pr-2 py-2 gap-3 text-sm text-zinc-400 data-[active=true]:text-emerald-400 enabled:hover:text-zinc-100">
+                <div className="flex items-center gap-4">
+                  <Video className="w-4 h-4 text-zinc-500" />
+
+                  <span>
+                    Vídeo
+                    1
+                  </span>
+                </div>
+                <span className="ml-auto font-mono text-xs text-zinc-500">
+                  03:54
+                </span>
+              </button>
+              <button className="pr-2">
+                <DotsThreeVertical className="w-4 h-4 text-zinc-500" />
+              </button>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
   )
 }
