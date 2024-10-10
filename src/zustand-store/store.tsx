1360efbf6e6cbe0a49fb9a4d5087f03e339c1a9c
joaogamin <joaumxp@hotmail.com>
2024-10-10 17:51:35 -0300

    jgamin : feat install and configure zustand store for global state approach

---
+++ src/zustand-store/store.tsx
@@ -0,0 +1,57 @@
+import { create } from 'zustand'
+
+type Video = {
+  id: string
+  title: string
+  duration: string
+  channel: string
+}
+
+export interface PlayerState {
+  videos: Array<Video>
+  currentVideoIndex: number
+
+  isLoading: boolean
+
+  setVideos: (videos: Array<Video>) => void
+  setIsLoading: (isLoading: boolean) => void
+
+  play: (videoIndex: [number]) => void
+  next: () => void
+}
+
+export const usePlayerStore = create<PlayerState>((set, get) => ({
+  videos: [],
+  currentVideoIndex: 0,
+  isLoading: true,
+
+  setIsLoading: (isLoading: boolean) => {
+    set({ isLoading })
+  },
+
+  setVideos: (videos: Array<Video>) => {
+    set({ videos })
+  },
+
+  play: (currentVideoIndex: [number]) => {
+    const [videoIndex] = currentVideoIndex
+    set({
+      currentVideoIndex: videoIndex,
+    })
+  },
+
+  next: () => {
+    const { videos, currentVideoIndex } = get()
+    const nextVideoIndex = currentVideoIndex + 1
+    const nextVideo = videos[nextVideoIndex]
+    if (nextVideo) {
+      set({ currentVideoIndex: nextVideoIndex })
+    } else {
+      const nextVideoIndex = currentVideoIndex + 1
+      const nextVideo = videos[nextVideoIndex]
+      if (nextVideo) {
+        set({ currentVideoIndex: 0 })
+      }
+    }
+  },
+}))
