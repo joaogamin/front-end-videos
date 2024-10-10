c8a4584d75799ad3ecdebedd66d2273109d950a8
joaogamin <joaumxp@hotmail.com>
2024-10-10 17:05:00 -0300

    jgamin : lint fix all files with new eslint rule

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,37 @@
-import type { Metadata } from "next";
-import { Inter } from 'next/font/google';
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter =
+  Inter(
+    {
+      subsets:
+        [
+          'latin',
+        ],
+    },
+  )
 
-
-export const metadata: Metadata = {
-  title: "YouTube URL Manager",
-};
+export const metadata: Metadata =
+  {
+    title:
+      'YouTube URL Manager',
+  }
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
       <body
         className={`${inter.className} antialiased`}
       >
-        {children}
+        {
+          children
+        }
       </body>
     </html>
-  );
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
-
 export default function HomePage() {
   return (
-   <h1>Hello World!</h1>
-  );
+    <h1>
+      Hello
+      World!
+    </h1>
+  )
 }
